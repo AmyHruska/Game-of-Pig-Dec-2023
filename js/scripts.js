73141863e06d915918b2a0e88172eedab0deb2d0
AmyHruska <amyhruskacda@gmail.com>
2023-12-06 12:00:19 -0800

    add end of day progress

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
 //     this.gamesWon = gamesWon
 //     this.turnTotal = turnTotal;
 //     this.activePlayer = activePlayer;
-//     this.playing = playing;
 // }
 
-function Game(score, turnTotal, activePlayer) {
+function Game(score, turnTotal, activePlayer, roll) {
     this.score = score;
     this.turnTotal = turnTotal;
     this.activePlayer = activePlayer;
+    this.roll = roll;
 }
 
 Game.prototype.turnSwap = function () {
@@ -18,13 +18,12 @@ Game.prototype.turnSwap = function () {
 };
 
 Game.prototype.rollDice = function () {
-    const roll = Math.trunc(Math.random() * 6) + 1;
-    if (roll === 1) {
+    this.roll = Math.trunc(Math.random() * 6) + 1;
+    if (this.roll === 1) {
         this.turnSwap();
     } else {
-        this.turnTotal += roll;
+        this.turnTotal += this.roll;
     }
-    return roll;
 };
 
 Game.prototype.hold = function () {
@@ -40,44 +39,67 @@ Game.prototype.gameWinCheck = function () {
 };
 
 // UI logic
-let players = new Game([0,0],0,0);
+let players = new Game([0,0],0,0,0);
 
-const diceEl = document.getElementById('diceImage');
+const diceImg = document.getElementById('diceImage');
 const btnNew = document.getElementById('newGameButton');
 const btnRoll = document.getElementById('rollButton');
 const btnHold = document.getElementById('holdButton');
 
 // UI function:
-const winButtonDisable =  function(){
+const winButtonDisable =  function() {
     btnRoll.disabled = true;
     btnHold.disabled = true;
 }
 
-// const newGameReset = function() {
-//     players = ([0,0],0,0);
-// }
-
-// Event listeners for new game, roll, and hold buttons
-btnNew.addEventListener('click', function () {
-    btnRoll.disabled = false;
-    btnHold.disabled = false;
-    // newGameReset();
-});
-
-btnRoll.addEventListener('click', function () {
-    players.rollDice();
+const scoreUpdate = function() {
     let activePlayer = players.activePlayer
     if (activePlayer === 0) {
     document.getElementById('current--0').innerText = players.turnTotal;
     } else {
     document.getElementById('current--1').innerText = players.turnTotal;
     }
+}
+
+const activePlayerUpdate = function () {
+    let activePlayer = players.activePlayer
+    if (activePlayer === 0) {
+        document.getElementById('current--0').innerText = players.turnTotal;
+        document.getElementById('p1Header').classList.add('active');
+        document.getElementById('p2Header').classList.remove('active');
+        } else {
+        document.getElementById('current--1').innerText = players.turnTotal;
+        document.getElementById('p2Header').classList.add('active');
+        document.getElementById('p1Header').classList.remove('active');
+        }
+};
+
+// Event listeners for new game, roll, and hold buttons
+btnNew.addEventListener('click', function () {
+    btnRoll.disabled = false;
+    btnHold.disabled = true;
+    players.score = [0,0];
+    scoreUpdate();
+    activePlayerUpdate();
+});
+
+btnRoll.addEventListener('click', function () {
+    players.rollDice();
+    scoreUpdate();
+    activePlayerUpdate();
+    const imgDiceResult = players.roll;
+    diceImg.src = `images/dice-${imgDiceResult}.png`;
+    let turnCheck = players.turnTotal;
+    if (turnCheck === 0) {
+        btnHold.disabled = true;
+    } else {
+        btnHold.disabled = false;
+    }
 });
 
 btnHold.addEventListener('click', function () {
     players.hold();
-    document.getElementById('current--0').innerText = 0;
-    document.getElementById('current--1').innerText = 0;
-    document.getElementById('total--0').innerText = players.score[0];
-    document.getElementById('total--1').innerText = players.score[1];
+    btnHold.disabled = true;
+    scoreUpdate();
+    activePlayerUpdate();    
 });
